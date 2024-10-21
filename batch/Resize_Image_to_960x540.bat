42cecb778193f9b05a00b193171757e46655c294
Shinrin <satoushinrin@gmail.com>
2024-10-21 17:53:37 +0700

    Upgrade Image Resizer

--- batch/Resize_Image_to_960x540.bat
+++ batch/Resize_Image_to_960x540.bat
@@ -2,37 +2,53 @@
 setlocal enabledelayedexpansion
 
 set "ffmpeg_path=C:\Tools\bin\ffmpeg.exe"
+set "ffprobe_path=C:\Tools\bin\ffprobe.exe"
 
-:: Check ffmpeg exists
-if not exist %ffmpeg_path% (
-    echo There is no ffmpeg, please install or update it!
+:: Check if ffmpeg exists
+if not exist "%ffmpeg_path%" (
+    echo ffmpeg not found! Please check the path.
+    pause
+    exit /b
+)
+
+:: Check if ffprobe exists
+if not exist "%ffprobe_path%" (
+    echo ffprobe not found! Please check the path.
     pause
     exit /b
 )
 
 :: Check if no file was selected
 if "%~1"=="" (
-    echo Please select at least one video file for conversion.
+    echo Please select an image file to merge.
     pause
     exit /b
 )
 
-:: Send files to cmd
+:: Set watermark path
+set "watermark=C:\Tools\preset\image\h_watermark.png"
+
+:: Executed
 for %%a in (%*) do (
-    :: Check output directory exists
-    set "input_file=%%~a"
-    set "input_dir=%%~dpa"
+    set "file=%%~a"
+    set "filename=%%~na"
+    set "dir=%%~dpa"
     
-    :: Create the output directory input file's directory
-    if not exist "%input_dir%Converted" (
-        mkdir "%input_dir%Converted"
+    :: Check output directory exists
+    if not exist "!dir!Converted" (
+        mkdir "!dir!Converted"
     )
     
+    :: Get the width/height of the input image
+    for /f "tokens=1,2 delims=," %%b in ('%ffprobe_path% -v error -select_streams v:0 -count_packets -show_entries stream^=width^,height -of csv^=p^=0 "%%~a"') do (
+        set "width=%%b"
+        set "height=%%c"
+    )
+    
+    :: Rename output
     set "rename=h-%%~na"
     set "rename=!rename: =-!"
     
-    :: Use ffmpeg to resize the image
-    "%ffmpeg_path%" -i "%%~a" -vf "scale=960:540" -q:v 1 "%input_dir%Converted\!rename!.jpg"
-
+    %ffmpeg_path% -i "%%~a" -i "%watermark%" -filter_complex "[1:v]scale=!width!:!height![wm];[0:v][wm]overlay=0:0,scale=960:540" -q:v 1 "!dir!Converted\!rename!.jpg"
 )
 exit /b
---
+++ preset/old_batch/Resize_Image_to_960x540.bat
@@ -0,0 +1,38 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "ffmpeg_path=C:\Tools\bin\ffmpeg.exe"
+
+:: Check ffmpeg exists
+if not exist %ffmpeg_path% (
+    echo There is no ffmpeg, please install or update it!
+    pause
+    exit /b
+)
+
+:: Check if no file was selected
+if "%~1"=="" (
+    echo Please select at least one video file for conversion.
+    pause
+    exit /b
+)
+
+:: Send files to cmd
+for %%a in (%*) do (
+    :: Check output directory exists
+    set "input_file=%%~a"
+    set "input_dir=%%~dpa"
+    
+    :: Create the output directory input file's directory
+    if not exist "%input_dir%Converted" (
+        mkdir "%input_dir%Converted"
+    )
+    
+    set "rename=h-%%~na"
+    set "rename=!rename: =-!"
+    
+    :: Use ffmpeg to resize the image
+    "%ffmpeg_path%" -i "%%~a" -vf "scale=960:540" -q:v 1 "%input_dir%Converted\!rename!.jpg"
+
+)
+exit /b
